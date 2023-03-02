6fbfcd8962ff6acd793b11a7abe71ba630d0c3f2
RichardbrobbeyASC20 <richardbrobbey2003@Yahoo.com>
2023-03-01 22:57:48 -0500

    equals function

--- lsp-spring-2023/src/org/howard/edu/hw4/IntegerSet.java
+++ lsp-spring-2023/src/org/howard/edu/hw4/IntegerSet.java
@@ -1,6 +1,20 @@
 //Richard Brobbey
 package org.howard.edu.hw4;
+import java.util.ArrayList;
 
 public class IntegerSet{
+	ArrayList<Integer> set = new ArrayList<Integer>();
+	
+	public IntegerSet(ArrayList list)
+	{
+		this.set = list;
+	}
+	public void clear()
+	{
+		set.clear();
+	}
+	public int length() {
+		return set.size();
+	}
 	
 }

638f9b14db3e803ac288c4fb6ef8267a54805add
RichardbrobbeyASC20 <richardbrobbey2003@Yahoo.com>
2023-03-02 18:32:12 -0500

    using all methods

--- lsp-spring-2023/src/org/howard/edu/hw4/Driver.java
+++ lsp-spring-2023/src/org/howard/edu/hw4/Driver.java
@@ -1,7 +1,26 @@
 //Richard Brobbey
 package org.howard.edu.hw4;
-
+import java.util.*;
 public class Driver
 {
-	
+	public static void main(String[] args)
+	{
+		ArrayList<Integer> list1 = new ArrayList<>();
+		ArrayList<Integer> list2 = new ArrayList<>();
+		IntegerSet set1 = new IntegerSet(list1);
+		IntegerSet set2 = new IntegerSet(list2);
+		set1.add(1);
+		set1.add(2);
+		set1.add(3);
+		System.out.println("Value of set1 is: " + set1.toString());
+		System.out.println("Smallest value in set1 is: " + set1.smallest());
+		System.out.println("Largest value in set1 is: " + set1.largest());
+		
+		set2.add(4);
+		set2.add(5);
+		
+		System.out.println("Union of set1 and set2 " + set1.union(set2));
+		System.out.println("Value of set2 is: " + set2.toString());
+		System.out.println("The difference of set1 and set2 is  " + set1.diff(set2));
+	}
 }

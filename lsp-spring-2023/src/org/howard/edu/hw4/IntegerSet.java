3546e334da6c6e14577cafd106a346728892ef23
RichardbrobbeyASC20 <richardbrobbey2003@Yahoo.com>
2023-03-02 17:26:52 -0500

    contains function

--- lsp-spring-2023/src/org/howard/edu/hw4/IntegerSet.java
+++ lsp-spring-2023/src/org/howard/edu/hw4/IntegerSet.java
@@ -1,6 +1,7 @@
 //Richard Brobbey
 package org.howard.edu.hw4;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class IntegerSet{
 	ArrayList<Integer> set = new ArrayList<Integer>();
@@ -18,10 +19,24 @@ public class IntegerSet{
 	}
 	public boolean equals(IntegerSet b)
 	{
-		ArrayList<Integer> set2 = b.list;
+		ArrayList<Integer> set2 = b.set;
 		if(set.size() != set2.size())
 		{
 			return false;
 		}
+		ArrayList<Integer> temp = set;
+		ArrayList<Integer> temp2 = set2;
+		Collections.sort(temp);
+		Collections.sort(temp2);
+		
+		for(int i = 0; i < temp.size(); i++)
+		{
+			if(temp.get(i) != temp2.get(i))
+			{
+				return false;
+			}
+		}
+		return true;
 	}
+	
 }
